e13157906857ad994cc452b393d0bb246dd26e9f
fahmiaditya16 <fahmiaditya1625@gmail.com>
2022-12-06 11:00:04 +0700

    tugas

---
+++ webportofolio/tugas.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Personal Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <section>
+        <!-- navigation -->
+        <nav>
+            <!-- logo -->
+            <a href="#" class="logo">Portfolio Tutorial</a>
+            <!-- menu -->
+            <ul>
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Testimonials</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+
+        <!-- text -->
+        <div class="text-container">
+            <p>Helloo</p>
+            <p>I&#8217;M Fahmi Aditya</p>
+            <p>CEO Technical Writer & Developer</p>
+            <p>An aquarius girl who loves music, watching movies and
+            <br>od course writing.</p>
+            <button class="hire-btn">Hire me</button>
+            <button class="down-cv">Download CV</button>
+        </div>
+        <!-- model -->
+        <img alt="model" class="model" src="./image/Problem solving-rafiki.png">
+    </section>
+
+    <!-- services -->
+    <div class="services">
+        <!-- text -->
+        <div class="services-text">
+            <p>Services</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempora aspernatur soluta a, libero nobis, itaque doloribus praesentium excepturi placeat assumenda cupiditate adipisci accusantium quasi, consequuntur voluptatibus sunt est alias?</p>
+        </div>
+
+        <div class="box-container">
+        <!-- 1 -->
+        <div class="box-1">
+            <span>1</span>
+            <p class="heading">technical writing</p>
+            <p class="details">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia sequi esse molestiae accusantium voluptate eos distinctio cupiditate sunt in eveniet! Ipsa maiores debitis officiis, voluptatem nam mollitia repellat voluptate minima.</p>
+            <button>Read More</button>
+        </div>
+        <!-- 2 -->
+        <div class="box-2">
+            <span>2</span>
+            <p class="heading">Web Design</p>
+            <p class="details">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi eligendi dicta non laudantium, nam delectus, placeat explicabo inventore quae rerum ab aut, praesentium nisi id consequatur odio tempora hic illo!</p>
+            <button>Read More</button>
+        </div>
+        <!-- 3 -->
+        <div class="box-3">
+            <span>3</span>
+            <p class="heading">Web Development</p>
+            <p class="details">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui quae, maiores tempora unde suscipit laboriosam quasi commodi dolorem cumque, impedit id explicabo dicta rem. Autem accusamus iure obcaecati doloribus iste.</p>
+            <button>Read More</button>
+        </div>
+    </div>
+</div>
+<!-- testimonials -->
+<div class="testimoni">
+    <!-- text -->
+    <div class="testimoni-text">
+        <p>What Our Client says..</p>
+    </div>
+
+    <div class="testimoni-col">
+        <div class="testimoni-1">
+            <img src="./image/Problem solving-pana.png">
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni minus beatae maiores consequuntur itaque perspiciatis ipsum ducimus a distinctio voluptates doloremque inventore tenetur, libero reprehenderit quam est eos eius eligendi!</p>
+                <h3>Client Pertama</h3>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+        </div>
+
+        <div class="testimoni-2">
+            <img src="./image/Deforestation-amico.png">
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni minus beatae maiores consequuntur itaque perspiciatis ipsum ducimus a distinctio voluptates doloremque inventore tenetur, libero reprehenderit quam est eos eius eligendi!</p>
+                <h3>Client Kedua</h3>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- footer -->
+<footer>
+    <p>Interested In Using Our Services?</p>
+
+    <!-- paragraph -->
+    <p>I try to work on your project as quickly as possible. Guarantee for revision until you are satisfied with my work.</p>
+    <!-- social -->
+    <div class="social-icons">
+        <a href="#"><i class="fab fa-whatsapp"></i></a>
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-youtube"></i></a>
+    </div>
+
+    <!-- copyright -->
+    <p class="copyright">Copyright by Fahmi Tutorial</p>
+</footer>
+
+<!-- social-attach-bar -->
+<div class="social">
+        <a href="#"><i class="fab fa-whatsapp"></i></a>
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-youtube"></i></a>
+    </div>
+</body>
+
+</html>
